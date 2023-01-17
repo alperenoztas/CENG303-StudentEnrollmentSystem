a835314f0e628e868bb2a8b6fcf31e536282c778
Alperen Öztaş <alprnoztas@gmail.com>
2023-01-17 21:54:11 +0300

    Scheduler class has fixed due to given instruction in the project and commented for further review.

--- src/main/java/org/example/Scheduler.java
+++ src/main/java/org/example/Scheduler.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.*;
 
-public class Scheduler {
+class Scheduler {
     private List<Classroom> classrooms;
     private University university;
     private Random rand = new Random();
@@ -13,21 +13,31 @@ public class Scheduler {
     }
 
     public void createSchedule() {
+
+        //We created a map to store the schedule for the classroom
         Map<Classroom, Map<String, List<Course>>> schedule = new HashMap<>();
 
+        //Initialized schedule for each classroom in the AYBU
         for (Classroom classroom : classrooms) {
             schedule.put(classroom, new HashMap<>());
         }
 
+        // For each department (Computer,Electric,Mechanical,Industrial,Construction)
         for (Department department : university.getDepartments()) {
+            // Get the courses given by the department in the AYBU
             List<Course> courses = department.getCourses();
+
+            // We sorted the courses here by credit hours
             Collections.sort(courses, new Comparator<Course>() {
                 @Override
                 public int compare(Course c1, Course c2) {
                     return Integer.compare(c1.getCreditHours(), c2.getCreditHours());
                 }
             });
+
+            // We assigned the courses to the classrooms
             for (Course course : courses) {
+                //For each course, a random classroom is chosen
                 Classroom classroom = classrooms.get(rand.nextInt(classrooms.size()));
                 Map<String, List<Course>> classroomSchedule = schedule.get(classroom);
                 boolean assigned = false;
@@ -48,12 +58,15 @@ public class Scheduler {
                     }
                 }
                 if (!assigned) {
-                    Classroom newClassroom = new Classroom(classrooms.size()+1, 50);
+                    // If there is no available time slot for the course,we increase the number of classrooms with 5 given in the assignment
+                    Classroom newClassroom = new Classroom(classrooms.size()+5, 50);
                     classrooms.add(newClassroom);
                     schedule.put(newClassroom, new HashMap<>());
                 }
             }
         }
+
+        // We assigned the schedule to each classroom
         for (Map.Entry<Classroom, Map<String, List<Course>>> entry : schedule.entrySet()) {
             Classroom classroom = entry.getKey();
             Map<String, List<Course>> classroomSchedule = entry.getValue();
